2734f22888a1d3b3cc83fb1bec9e96245f6b3225
YE GAO <sortsimilar@gmail.com>
2023-04-25 13:16:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# 这是一个示例 Python 脚本。
-
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+import tkinter as tk
+import time
 
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+class Clock(tk.Label):
+    def __init__(self, parent=None, seconds=True, **kw):
+        tk.Label.__init__(self, parent, kw)
+        self.display_seconds = seconds
+        self.update_time()
+        self.pack()
+
+    def update_time(self):
+        if self.display_seconds:
+            format = '%H:%M:%S'
+        else:
+            format = '%H:%M'
+        current_time = time.strftime(format)
+        self.config(text=current_time)
+        self.after(1000, self.update_time)
 
 
-# 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
+    root = tk.Tk()
+    clock = Clock(root, font=('Helvetica', 48), seconds=True)
+    root.mainloop()
